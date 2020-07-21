414127e9596df752d957a8c6add329f115de96ed
Daniel Ostashev <d.ostashev@yandex.ru>
2020-07-21 17:01:41 +0300

    Add basic home page UI (#2)
    * Add basic home page UI
    
    * Fix semicolon absence in script.js
    
    Co-authored-by: Christian Pellegrin <chripell@gmail.com>
    
    * Return the license in script.js
    
    Co-authored-by: Christian Pellegrin <chripell@gmail.com>

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,17 +12,65 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+class ArticleCard {
+  constructor(title, text, photoURL, url) {
+    this.title = title;
+    this.text = text;
+    this.photoURL = photoURL;
+    this.url = url;
+  }
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  get html() {
+    let card = document.createElement("div");
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+    card.className = "article-card";
+    card.innerHTML = `
+      <img src="${this.photoURL}">
+      <div class="content">
+        <h1>${this.title}</h1>
+        <p>${this.text}</p>
+      </div>
+    `;
+    card.onclick = () => { document.location = this.url; };
+
+    let cardWrap = document.createElement("div");
+    cardWrap.className = "card-wrap";
+
+    cardWrap.appendChild(card);
+
+    return cardWrap;
+  };
+}
+
+window.onload = () => {
+ 
+  // TODO: Add getting articles from backend
+  let cards = [
+    new ArticleCard(
+      "Genetic algorithm for distribuition of delivery routes",
+      "This was my project in several paper defense competitions.",
+      "static/MDVRP.png",
+      "static/Genetic algorithm.pdf",
+    ),
+    new ArticleCard(
+      `"Guki" game`,
+      "This is a PC game with mechanics similar to Magicka that I was developing (though unfortunately didn't finish) some time ago.",
+      "static/Guki.png",
+      "https://github.com/Omn1/MagicGame",
+    ),
+    new ArticleCard(
+      "Electronic circuit editor",
+      "This is a project that I (mostly) developed for a national competition.",
+      "static/Electronic-circuit-editor.png",
+      "https://github.com/Omn1/Electronic-circuit-editor",
+    ),
+    new ArticleCard(
+      "Working with input fields on iOS",
+      "This is an article I wrote based on my experience working on an iOS app.",
+      "static/Input fields.png",
+      "https://cruxlab.com/blog/working-with-input-fields-on-ios/",
+    )
+  ];
+
+  cards.forEach(card => document.getElementById("articles-container").appendChild(card.html));
 }
